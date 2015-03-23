162b78e4d9c82fc070afb6ba9ef5a3577f98433f
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2015-03-23 00:59:23 -0500

    Se modificó el readme y se cambió el menú principal

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 Aplicación desarrollada paso a paso en video.
 
 Esta es una línea agregada desde github en el navegador.
+
+Esta línea la agregé desde mi computador.
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="#">Cilindros</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -40,8 +40,8 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="{!! url('auth/login') !!}">Login</a></li>
-						<li><a href="{!! url('auth/register') !!}">Register</a></li>
+						<li><a href="{!! url('auth/login') !!}">Entrar</a></li>
+						<li><a href="{!! url('auth/register') !!}">Registrarme</a></li>
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
